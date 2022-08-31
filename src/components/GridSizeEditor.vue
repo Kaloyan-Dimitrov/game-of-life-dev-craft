5ee02754ec85ac4112b0d2ae0f1d7965f9d8c3b2
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2022-08-31 21:43:10 +0300

    Styled grid size editor

--- src/components/GridSizeEditor.vue
+++ src/components/GridSizeEditor.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <input v-model.lazy.number="store.width" />
-    <input v-model.lazy.number="store.height" />
+  <div class="wrapper">
+    <input class="dim-input" v-model.lazy.number="store.width" />
+    <div class="X">X</div>
+    <input class="dim-input" v-model.lazy.number="store.height" />
   </div>
 </template>
 
@@ -16,4 +17,29 @@ const width: Ref<number> = ref(store.width);
 const height: Ref<number> = ref(store.width);
 </script>
 
-<style scoped></style>
+<style scoped>
+.dim-input {
+  width: 50%;
+  background: none;
+  border-color: var(--green-color);
+  color: var(--green-color);
+  border-radius: 0.9em;
+  padding: 0 1em 0 1em;
+  margin: 1em;
+  border-style: solid;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  font-size: 1.2em;
+  line-height: 2.3;
+}
+
+.wrapper {
+  display: flex;
+}
+
+.wrapper > .X {
+  margin: auto;
+  text-align: center;
+}
+</style>
