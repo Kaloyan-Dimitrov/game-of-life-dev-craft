fdb7cd3204771fdcf01d96d27580a1c0f2abfbcc
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2022-09-01 01:21:42 +0300

    Added stats

---
+++ frontend/src/components/StatsComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="statsWrapper">
+    <div class="label">Ticks:</div>
+    <div class="label">Alive cells:</div>
+    <div class="label">FPS:</div>
+    <div class="stat">{{ store.numTicks }}</div>
+    <div class="stat">{{ store.aliveCellsNum }}</div>
+    <div class="stat">{{ store.FPS }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGameStore } from "@/stores/game";
+
+const store = useGameStore();
+</script>
+
+<style scoped>
+.statsWrapper {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  text-align: center;
+  margin: 1em;
+}
+.stat {
+  color: var(--green-color);
+  font-size: 3ch;
+}
+</style>
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -2,13 +2,14 @@
 import GameComponent from "../components/GameComponent.vue";
 import GridSizeEditor from "../components/GridSizeEditor.vue";
 import ControlsComponent from "../components/ControlsComponent.vue";
+import StatsComponent from "../components/StatsComponent.vue";
 import { useGameStore } from "@/stores/game";
 const store = useGameStore();
 </script>
 
 <template>
   <div>
-    <!-- <StatsComponent></StatsComponent> -->
+    <StatsComponent></StatsComponent>
     <GameComponent :width="store.width" :height="store.height"></GameComponent>
     <GridSizeEditor></GridSizeEditor>
     <ControlsComponent></ControlsComponent>
